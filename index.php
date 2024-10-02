35e84b1a588a1f4b56c6696f44e8badba63a91de
bellagordon <157334313+bellagordon@users.noreply.github.com>
2024-10-02 10:01:31 -0500

    Update index.php

--- index.php
+++ index.php
@@ -5,4 +5,12 @@ include "view-header.php";
     <h1>MIS 4013 Homework 3</h1>
 <?php
 include "view-footer.php";
+
 ?>
+<?php
+error_reporting(E_All);
+ini_set('display_errors',1);
+include 'utl-db.php';
+?>
+
+
