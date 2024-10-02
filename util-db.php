8c4f2a4c43f17df3ebbe9387dc278ec51929818f
bellagordon <157334313+bellagordon@users.noreply.github.com>
2024-10-02 11:20:36 -0500

    Update util-db.php

--- util-db.php
+++ util-db.php
@@ -1,6 +1,6 @@
 <?php
 function get_db_connection(){
-$conn = new mysqli(’gordonmis4013hw3.mysql.database.azure.com’, ‘gord0065’, ‘Pinkey227!’, ‘hw3_database’);
+$conn = new mysqli(’gordonmis4013hw3.mysql.database.azure.com’, ‘gord0065’, "Pinkey227!", ‘hw3_database’);
   }
  if ($conn->connect_error) {
       return false;
