8e296fd675fc4af6f2f49bd981988c7b93fa6a66
bellagordon <157334313+bellagordon@users.noreply.github.com>
2024-10-02 02:36:27 -0500

    Create courses-by-instructor.php

---
+++ courses-by-instructor.php
@@ -0,0 +1,10 @@
+<?php
+require_once("util-db.php");
+require_once("model-courses-by-instructor.php");
+
+$pageTitle = "Courses by instructor";
+include "view-header.php";
+$courses = selectCoursesbyinstructor($_GET['id']);
+include "view-courses-by-instructor.php";
+include "view-footer.php";
+?>
