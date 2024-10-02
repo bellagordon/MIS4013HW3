418b8507b4efef0fcb82b9da13c1ab3240ff8478
bellagordon <157334313+bellagordon@users.noreply.github.com>
2024-10-02 12:26:58 -0500

    Update courses-by-instructor.php

--- courses-by-instructor.php
+++ courses-by-instructor.php
@@ -4,7 +4,7 @@ require_once("model-courses-by-instructor.php");
 
 $pageTitle = "Courses by instructor";
 include "view-header.php";
-$course = selectCoursesbyinstructor($_GET['id']);
+$courses = selectCoursesbyinstructor($_GET['id']);
 include "view-courses-by-instructor.php";
 include "view-footer.php";
 ?>
