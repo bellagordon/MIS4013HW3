908a5d305214a62c6dbdb67b24a9d74ce111947c
bellagordon <157334313+bellagordon@users.noreply.github.com>
2024-10-09 13:42:24 -0500

    Update model-sections-by-course.php

--- model-sections-by-course.php
+++ model-sections-by-course.php
@@ -2,7 +2,7 @@
 function selectSectionsByCourse($cid) {
     try {
         $conn = get_db_connection();
-        $stmt = $conn->prepare("SELECT c.course_id, course_number,course_description, semester, room, day_time FROM course c join section s on s.course_id = c.course_id where s.course_id=?");
+        $stmt = $conn->prepare("SELECT c.patient_id, patient_name, patient_description, room, day_time FROM course c join section s on s.patient_id = c.patient_id where s.patient_id=?");
         $stmt->bind_param("i", $cid);
         $stmt->execute();
         $result = $stmt->get_result();
