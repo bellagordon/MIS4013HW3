e022c135e60206e61127372e4df4b4bd7d256a03
bellagordon <157334313+bellagordon@users.noreply.github.com>
2024-10-09 13:42:58 -0500

    Update view-courses-by-instructor.php

--- view-courses-by-instructor.php
+++ view-courses-by-instructor.php
@@ -17,10 +17,10 @@
 while ($course = $courses->fetch_assoc()) {
   ?>
   <tr>
-    <td><?php echo $course['course_id']; ?></td>
-    <td><?php echo $course['course_number']; ?></td>
-    <td><?php echo $course['course_description']; ?></td>
-    <td><?php echo $course['semester']; ?></td>
+    <td><?php echo $course['patient_id']; ?></td>
+    <td><?php echo $course['patient_name']; ?></td>
+    <td><?php echo $course['patient_description']; ?></td>
+    
     <td><?php echo $course['room']; ?></td>
     <td><?php echo $course['day_time']; ?></td>
   </tr>
