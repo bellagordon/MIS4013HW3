2656b4ae68735c166819fd113fc53aa545aeb3d0
bellagordon <157334313+bellagordon@users.noreply.github.com>
2024-10-09 13:43:54 -0500

    Update view-instructors-with-courses.php

--- view-instructors-with-courses.php
+++ view-instructors-with-courses.php
@@ -7,14 +7,14 @@ while ($instructor = $instructors->fetch_assoc()) {
   ?>
   <div class="card">
     <div class="card-body">
-      <h5 class="card-title"><?php echo $instructor['instructor_name']; ?></h5>
+      <h5 class="card-title"><?php echo $instructor['doctor_name']; ?></h5>
       <p class="card-text">
       <ul class="list-group">
   <?php
   $courses = selectCoursesByInstructor($instructor['instructor_id']);
   while ($course = $courses ->fetch_assoc()) {
     ?>
-      <li class="list-group-item"><?php echo $course['course_number']; ?> - <?php echo $course['semester']; ?> - <?php echo $course['room']; ?> - <?php echo $course['day_time']; ?> </li>
+      <li class="list-group-item"><?php echo $course['patient_name']; ?> - <?php echo $course['room']; ?> - <?php echo $course['day_time']; ?> </li>
 
     <?php
   }
