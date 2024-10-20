1fecd64e7fd10fe6726e302920828e43e112af12
bellagordon <157334313+bellagordon@users.noreply.github.com>
2024-10-20 16:13:51 -0500

    Update doctor-by-chief.php

--- doctor-by-chief.php
+++ doctor-by-chief.php
@@ -4,7 +4,7 @@ require_once("model-doctor-by-chief.php");
 
 $pageTitle = "Doctors assigned to Head Surgeon";
 include "view-header.php";
-$doctors = selectDoctorsByChief($_POST['cid']);
+$doctors = selectDoctorsByChief($_POST['iid']);
 include "view-doctor-by-chief.php";
 include "view-footer.php";
 ?>
