ac0ab0c6e9b55e0e654ad9323220d01538cd7159
bellagordon <157334313+bellagordon@users.noreply.github.com>
2024-10-20 15:30:30 -0500

    Update model-chiefs.php

--- model-chiefs.php
+++ model-chiefs.php
@@ -2,7 +2,7 @@
 function selectChiefs() {
     try {
         $conn = get_db_connection();
-        $stmt = $conn->prepare("SELECT chief_id, i.doctor_id, chief_name, chief_dpt FROM chief c JOIN instructors i ON c.doctorid = i.doctorid");
+        $stmt = $conn->prepare("SELECT chief_id, i.doctor_id, chief_name, chief_dpt FROM chief c JOIN instructor i ON c.doctorid = i.doctorid");
         $stmt->execute();
         $result = $stmt->get_result();
         $conn->close();
