36056b449f47eb0c49f39e3e9b83f0e1d54922a4
bellagordon <157334313+bellagordon@users.noreply.github.com>
2024-10-20 15:56:24 -0500

    Update view-chiefs.php

--- view-chiefs.php
+++ view-chiefs.php
@@ -1,26 +1,25 @@
-<h1>Head Surgeons with Doctors</h1>
-<div class="card-group">
-
-
+<h1>Head Surgeons</h1>
+<div class="table-responsive">
+  <table class="table">
+    <thead>
+      <tr>
+        <th>ID</th>
+      <th>Name</th>
+      <th>Department</th>
+        <th></th>
+      </tr>
+    </thead>
+    <tbody>
       <?php
 while ($chief = $chiefs->fetch_assoc()) {
   ?>
-  <div class="card">
-    <div class="card-body">
-      <h5 class="card-title"><?php echo $chief['chief_name']; ?></h5>
-      <p class="card-text">
-      <ul class="list-group">
-  <?php
-  $chiefs = selectChiefs($instructor['doctor_id']);
-  while ($instructor = $instructors ->fetch_assoc()) {
-    ?>
-      <li class="list-group-item"><?php echo $instructor['doctor_name']; ?> </li>
-
-    <?php
-  }
-  
- 
+  <tr>
+   <td><?php echo $chief['chief_id']; ?></td>
+    <td><?php echo $chief['chief_name']; ?></td>
+    <td><?php echo $chief['chief_dpt']; ?></td>
+     <td>
+       
+   </td>
+  </tr>
   <?php
 }
-        ?>
-  </div>
