6dcd49fac05719327a402ee2dfedd4e44387cb44
bellagordon <157334313+bellagordon@users.noreply.github.com>
2024-10-20 15:13:54 -0500

    Update and rename model-chief.php to model-chiefs.php

--- model-chiefs.php
+++ model-chiefs.php
@@ -1,8 +1,8 @@
 <?php
-function selectCourses() {
+function selectChiefs() {
     try {
         $conn = get_db_connection();
-        $stmt = $conn->prepare("SELECT patient_id, patient_name, patient_description FROM course");
+        $stmt = $conn->prepare("SELECT chief_id, i.doctor_id, chief_name, chief_dpt FROM chiefs c JOIN instructors i ON c.doctorid = i.doctorid WHERE i.doctorid =");
         $stmt->execute();
         $result = $stmt->get_result();
         $conn->close();
