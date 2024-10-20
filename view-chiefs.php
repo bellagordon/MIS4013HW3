9ed795582113628cbe8c6fadb73c51a3216549bd
bellagordon <157334313+bellagordon@users.noreply.github.com>
2024-10-20 15:50:38 -0500

    Update view-chiefs.php

--- view-chiefs.php
+++ view-chiefs.php
@@ -1,31 +1,31 @@
-<h1>Head Surgeons</h1>
-<div class="table-responsive">
-  <table class="table">
-    <thead>
-      <tr>
-        <th>ID</th>
-      <th>Name</th>
-      <th>Department</th>
-        <th></th>
-      </tr>
-    </thead>
-    <tbody>
+<h1>Head Surgeons with Doctors</h1>
+<div class="card-group">
+
+
       <?php
 while ($chief = $chiefs->fetch_assoc()) {
   ?>
-  <tr>
-   <td><?php echo $chief['chief_id']; ?></td>
-    <td><?php echo $chief['chief_name']; ?></td>
-    <td><?php echo $chief['chief_dpt']; ?></td>
-     <td>
+  <div class="card">
+    <div class="card-body">
+      <h5 class="card-title"><?php echo $chief['chief_name']; ?></h5>
+      <p class="card-text">
+      <ul class="list-group">
+  <?php
+  $chiefs = selectCoursesByInstructor($instructor['doctor_id']);
+  while ($course = $courses ->fetch_assoc()) {
+    ?>
+      <li class="list-group-item"><?php echo $course['patient_name']; ?> - <?php echo $course['room']; ?> - <?php echo $course['day_time']; ?> </li>
 
-        <form method="post" action="instructor.php">
-        
-        <input type = "hidden" name = "cid" value ="<?php echo $chief['doctor_id']; ?>">
-  <button type="submit" class="btn btn-primary">Assigned Doctor</button>
-      </form>
-       
-   </td>
-  </tr>
+    <?php
+  }
+  ?>
+      </ul>   
+      </p>
+      <p class="card-text"><small class="text-body-secondary">Office : <?php echo $instructor['office_number']; ?>></p>
+    </div>
+  </div>
+ 
   <?php
 }
+        ?>
+  </div>
