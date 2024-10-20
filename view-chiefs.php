048d17a78d55e58b0e06e92feb98ea7a4eb5d8c6
bellagordon <157334313+bellagordon@users.noreply.github.com>
2024-10-20 15:43:48 -0500

    Update view-chiefs.php

--- view-chiefs.php
+++ view-chiefs.php
@@ -18,11 +18,7 @@ while ($chief = $chiefs->fetch_assoc()) {
     <td><?php echo $chiefs['chief_name']; ?></td>
     <td><?php echo $chiefs['chief_dpt']; ?></td>
      <td>
-      <form method="post" action="instructor.php">
-        
-        <input type = "hidden" name = "cid" value ="<?php echo $chief['doctor_id']; ?>">
-  <button type="submit" class="btn btn-primary">Assigned doctor</button>
-      </form>
+      
    </td>
   </tr>
   <?php
