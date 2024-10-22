69e198b9eb262da1d5a5457c75252d9a8f2b19c1
bellagordon <157334313+bellagordon@users.noreply.github.com>
2024-10-22 14:32:50 -0500

    Update courses.php

--- courses.php
+++ courses.php
@@ -4,6 +4,16 @@ require_once("model-courses.php");
 
 $pageTitle = "Patients";
 include "view-header.php";
+
+if (isset($_POST['actionType'])) {
+  switch ($_POST['actionType']) {
+    case "Add":
+      insertCourse($_POST['cName'], $_POST['cDesc'];
+      break;
+    
+  }
+}
+
 $courses = selectCourses();
 include "view-courses.php";
 include "view-footer.php";
