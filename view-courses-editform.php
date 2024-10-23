6b9ac7a59079ce8e709ef63a6d745f08b4a6e345
bellagordon <157334313+bellagordon@users.noreply.github.com>
2024-10-23 13:22:16 -0500

    Update view-courses-editform.php

--- view-courses-editform.php
+++ view-courses-editform.php
@@ -7,11 +7,11 @@
 </button>
 
 
-<div class="modal fade" id="editCourseModal <?php echo $course['patient_id']; ?>" tabindex="-1" aria-labelledby="editCourseModalLabel <?php echo $course['patient_id']; ?>" aria-hidden="true">
+<div class="modal fade" id="editCourseModal<?php echo $course['patient_id']; ?>" tabindex="-1" aria-labelledby="editCourseModalLabel<?php echo $course['patient_id']; ?>" aria-hidden="true">
   <div class="modal-dialog">
     <div class="modal-content">
       <div class="modal-header">
-        <h1 class="modal-title fs-5" id="editCourseModalLabel <?php echo $course['patient_id']; ?>">Edit Patient</h1>
+        <h1 class="modal-title fs-5" id="editCourseModalLabel<?php echo $course['patient_id']; ?>">Edit Patient</h1>
         <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
       </div>
       <div class="modal-body">
