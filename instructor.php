1dc470d883ad6f12e1ce304a668301497cffe26f
bellagordon <157334313+bellagordon@users.noreply.github.com>
2024-10-23 00:08:59 -0500

    Update instructor.php

--- instructor.php
+++ instructor.php
@@ -4,6 +4,33 @@ require_once("model-instructors.php");
 
 $pageTitle = "Doctors";
 include "view-header.php";
+
+if (isset($_POST['actionType'])) {
+  switch ($_POST['actionType']) {
+    case "Add":
+      if (insertInstructor($_POST['iName'], $_POST['iDesc'])) {
+        echo '<div class="alert alert-success" role="alert"> Doctor added! </div>';
+      } else {
+         echo '<div class="alert alert-danger" role="alert"> Error </div>';
+      }
+      break;
+     case "Edit":
+      if (editInstructor($_POST['iName'], $_POST['cDesc'], $_POST['iid'])) {
+        echo '<div class="alert alert-success" role="alert"> Doctor edited! </div>';
+      } else {
+         echo '<div class="alert alert-danger" role="alert"> Error </div>';
+      }
+      break;
+     case "Delete":
+      if (deleteInstructor($_POST['iid'])) {
+        echo '<div class="alert alert-success" role="alert"> Doctor deleted! </div>';
+      } else {
+         echo '<div class="alert alert-danger" role="alert"> Error </div>';
+      }
+      break;
+  }
+}
+
 $instructors = selectInstructors();
 include "view-instructors.php";
 include "view-footer.php";
