28725d2ee75f543b3e252b56692825007087e863
bellagordon <157334313+bellagordon@users.noreply.github.com>
2024-10-23 12:17:45 -0500

    Update view-courses-newform.php

--- view-courses-newform.php
+++ view-courses-newform.php
@@ -9,11 +9,11 @@
 </button>
 
 <!-- Modal -->
-<div class="modal fade" id="newCourseModal" tabindex="-1" aria-labelledby="newCourseModallLabel" aria-hidden="true">
+<div class="modal fade" id="newCourseModal" tabindex="-1" aria-labelledby="newCourseModalLabel" aria-hidden="true">
   <div class="modal-dialog">
     <div class="modal-content">
       <div class="modal-header">
-        <h1 class="modal-title fs-5" id="newCourseModallLabel">New Patient</h1>
+        <h1 class="modal-title fs-5" id="newCourseModalLabel">New Patient</h1>
         <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
       </div>
       <div class="modal-body">
