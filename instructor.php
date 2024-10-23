c3caaa01f6918c8d049c9a04e19a4bd936fdf557
bellagordon <157334313+bellagordon@users.noreply.github.com>
2024-10-23 00:10:40 -0500

    Update instructor.php

--- instructor.php
+++ instructor.php
@@ -8,14 +8,14 @@ include "view-header.php";
 if (isset($_POST['actionType'])) {
   switch ($_POST['actionType']) {
     case "Add":
-      if (insertInstructor($_POST['iName'], $_POST['iDesc'])) {
+      if (insertInstructor($_POST['iName'], $_POST['iNum'])) {
         echo '<div class="alert alert-success" role="alert"> Doctor added! </div>';
       } else {
          echo '<div class="alert alert-danger" role="alert"> Error </div>';
       }
       break;
      case "Edit":
-      if (editInstructor($_POST['iName'], $_POST['cDesc'], $_POST['iid'])) {
+      if (editInstructor($_POST['iName'], $_POST['iNum'], $_POST['iid'])) {
         echo '<div class="alert alert-success" role="alert"> Doctor edited! </div>';
       } else {
          echo '<div class="alert alert-danger" role="alert"> Error </div>';
