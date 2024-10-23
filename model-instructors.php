cb67d77f6733cdc556867212a8de84f69843be4b
bellagordon <157334313+bellagordon@users.noreply.github.com>
2024-10-23 00:10:12 -0500

    Update model-instructors.php

--- model-instructors.php
+++ model-instructors.php
@@ -13,4 +13,47 @@ function selectInstructors() {
     }
 }
 
+function insertInstructor($iName, $iDesc) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("INSERT INTO `hw3_database`.`instructor` (`doctor_name`, `office_number`) VALUES ( ?, ?)");
+        $stmt->bind_param("ss", $cName, $cDesc);
+        $success = $stmt->execute();
+        $conn->close();
+        return $success;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+
+function editCourse($cName, $cDesc, $cid) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("UPDATE `hw3_database`.`course` set `patient_name` = ?, `patient_description` = ?  WHERE patient_id =?");
+        $stmt->bind_param("ssi",$cName, $cDesc, $cid);
+        $success = $stmt->execute();
+        $conn->close();
+        return $success;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+
+function deleteCourse($cid) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("DELETE FROM `hw3_database`.`course` WHERE patient_id = ?");
+        $stmt->bind_param("i",$cid);
+        $success = $stmt->execute();
+        $conn->close();
+        return $success;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+
+
 ?>
