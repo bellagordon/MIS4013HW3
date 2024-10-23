9a6d3d070be82a46e21af6a29b5cd822a8212bad
bellagordon <157334313+bellagordon@users.noreply.github.com>
2024-10-23 13:14:41 -0500

    Update view-instructors-editform.php

--- view-instructors-editform.php
+++ view-instructors-editform.php
@@ -5,7 +5,7 @@
 </button>
 
 
-<div class="modal fade" id="editInstructorModal <?php echo $course['doctor_id']; ?>" tabindex="-1" aria-labelledby="editInstructorModalLabel <?php echo $instructor['doctor_id']; ?>" aria-hidden="true">
+<div class="modal fade" id="editInstructorModal<?php echo $course['doctor_id']; ?>" tabindex="-1" aria-labelledby="editInstructorModalLabel <?php echo $instructor['doctor_id']; ?>" aria-hidden="true">
   <div class="modal-dialog">
     <div class="modal-content">
       <div class="modal-header">
