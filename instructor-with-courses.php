d5fba87e09758d3c6217174e747a7dff5ba648bc
bellagordon <157334313+bellagordon@users.noreply.github.com>
2024-10-23 12:05:57 -0500

    Update instructor-with-courses.php

--- instructor-with-courses.php
+++ instructor-with-courses.php
@@ -4,6 +4,33 @@ require_once("model-instructors-with-courses.php");
 
 $pageTitle = "Doctors with Patients";
 include "view-header.php";
+
+if (isset($_POST['actionType'])) {
+  switch ($_POST['actionType']) {
+    case "Add":
+      if (insertSection($_POST['iName'], $_POST['iNum'])) {
+        echo '<div class="alert alert-success" role="alert"> Patient added to Doctor! </div>';
+      } else {
+         echo '<div class="alert alert-danger" role="alert"> Error </div>';
+      }
+      break;
+     case "Edit":
+      if (editSection($_POST['iName'], $_POST['iNum'], $_POST['iid'])) {
+        echo '<div class="alert alert-success" role="alert"> Patient edited by Doctor! </div>';
+      } else {
+         echo '<div class="alert alert-danger" role="alert"> Error </div>';
+      }
+      break;
+     case "Delete":
+      if (deleteSection($_POST['iid'])) {
+        echo '<div class="alert alert-success" role="alert"> Patient deleted! </div>';
+      } else {
+         echo '<div class="alert alert-danger" role="alert"> Error </div>';
+      }
+      break;
+  }
+}
+
 $instructors = selectInstructors();
 include "view-instructors-with-courses.php";
 include "view-footer.php";
