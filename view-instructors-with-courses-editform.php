e8bf9101115f1e92bcddb6bd253944719bc5d890
bellagordon <157334313+bellagordon@users.noreply.github.com>
2024-10-28 13:20:43 -0500

    Update view-instructors-with-courses-editform.php

--- view-instructors-with-courses-editform.php
+++ view-instructors-with-courses-editform.php
@@ -1,4 +1,4 @@
-<button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#editSectionModal<?php echo $section['nurse_id']; ?>" >
+<button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#editSectionModal<?php echo $sections['nurse_id']; ?>" >
       <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pencil-fill" viewBox="0 0 16 16">
         <path d="M12.854.146a.5.5 0 0 0-.707 0L10.5 1.793 14.207 5.5l1.647-1.646a.5.5 0 0 0 0-.708zm.646 6.061L9.793 2.5 3.293 9H3.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.207zm-7.468 7.468A.5.5 0 0 1 6 13.5V13h-.5a.5.5 0 0 1-.5-.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.5-.5V10h-.5a.5.5 0 0 1-.175-.032l-.179.178a.5.5 0 0 0-.11.168l-2 5a.5.5 0 0 0 .65.65l5-2a.5.5 0 0 0 .168-.11z"/>
       </svg>
@@ -9,30 +9,30 @@
   <div class="modal-dialog">
     <div class="modal-content">
       <div class="modal-header">
-        <h1 class="modal-title fs-5" id="editSectionModalLabel<?php echo $section['nurse_id']; ?>">Edit Patient and Doctor</h1>
+        <h1 class="modal-title fs-5" id="editSectionModalLabel<?php echo $sections['nurse_id']; ?>">Edit Patient and Doctor</h1>
         <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
       </div>
       <div class="modal-body">
         <form method="post" action="">
             <div class="mb-3">
-              <label for="iid <?php echo $section['doctor_id']; ?>" class="form-label">Doctor</label>
-              <input type="text" class="form-control" id="iid <?php echo $section ['nurse_id']; ?>" name="iid" value="<?php echo $section['doctor_id']; ?>" >
+              <label for="iid <?php echo $sections['doctor_id']; ?>" class="form-label">Doctor</label>
+              <input type="text" class="form-control" id="iid <?php echo $sections['nurse_id']; ?>" name="iid" value="<?php echo $sections['doctor_id']; ?>" >
             </div>
            <div class="mb-3">
-              <label for="cid <?php echo $section['patient_id']; ?>" class="form-label">Patient</label>
-              <input type="text" class="form-control" id="cid <?php echo $section['nurse_id']; ?>" name="cid" value = "<?php echo $section['doctor_id']; ?>" >
+              <label for="cid <?php echo $sections['patient_id']; ?>" class="form-label">Patient</label>
+              <input type="text" class="form-control" id="cid <?php echo $sections['nurse_id']; ?>" name="cid" value = "<?php echo $sections['doctor_id']; ?>" >
             </div>
                <div class="mb-3">
-              <label for="sName <?php echo $section['nurse_name']; ?>" class="form-label">Nurse Name</label>
-              <input type="text" class="form-control" id="sName <?php echo $section['nurse_id']; ?>" name="sName" value = "<?php echo $section['doctor_id']; ?>" >
+              <label for="sName <?php echo $sections['nurse_name']; ?>" class="form-label">Nurse Name</label>
+              <input type="text" class="form-control" id="sName <?php echo $sections['nurse_id']; ?>" name="sName" value = "<?php echo $sections['doctor_id']; ?>" >
             </div> <div class="mb-3">
-              <label for="room <?php echo $section['room']; ?>" class="form-label">Room</label>
-              <input type="text" class="form-control" id="room <?php echo $section['nurse_id']; ?>" name="room" value = "<?php echo $section['doctor_id']; ?>" >
+              <label for="room <?php echo $sections['room']; ?>" class="form-label">Room</label>
+              <input type="text" class="form-control" id="room <?php echo $sections['nurse_id']; ?>" name="room" value = "<?php echo $sections['doctor_id']; ?>" >
             </div> <div class="mb-3">
-              <label for="time <?php echo $section['day_time']; ?>" class="form-label">Date and Time</label>
-              <input type="text" class="form-control" id="time <?php echo $section['nurse_id']; ?>" name="time" value = "<?php echo $section['doctor_id']; ?>" >
+              <label for="time <?php echo $sections['day_time']; ?>" class="form-label">Date and Time</label>
+              <input type="text" class="form-control" id="time <?php echo $sections['nurse_id']; ?>" name="time" value = "<?php echo $sections['doctor_id']; ?>" >
             </div>
-             <input type="hidden" name="sid" value = "<?php echo $section['nurse_id']; ?>" >
+             <input type="hidden" name="sid" value = "<?php echo $sections['nurse_id']; ?>" >
               <input type ="hidden" name="actionType" value="Edit">
             <button type="submit" class="btn btn-primary">Save</button>
        </form>
