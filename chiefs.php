670ee7dcb88ba0e242866f13321cfbfabfd1798c
bellagordon <157334313+bellagordon@users.noreply.github.com>
2024-11-04 13:30:45 -0600

    Update chiefs.php

--- chiefs.php
+++ chiefs.php
@@ -4,6 +4,33 @@ require_once("model-chiefs.php");
 
 $pageTitle = "Head Surgeons";
 include "view-header.php";
+
+if (isset($_POST['actionType'])) {
+  switch ($_POST['actionType']) {
+    case "Add":
+      if (insertChief($_POST['chName'], $_POST['chDpt'])) {
+        echo '<div class="alert alert-success" role="alert"> Chief surgeon added! </div>';
+      } else {
+         echo '<div class="alert alert-danger" role="alert"> Error </div>';
+      }
+      break;
+     case "Edit":
+      if (editChief($_POST['chName'], $_POST['chDpt'], $_POST['chid'])) {
+        echo '<div class="alert alert-success" role="alert"> Chief surgeon edited! </div>';
+      } else {
+         echo '<div class="alert alert-danger" role="alert"> Error </div>';
+      }
+      break;
+     case "Delete":
+      if (deleteCourse($_POST['chid'])) {
+        echo '<div class="alert alert-success" role="alert"> Chief surgeon deleted! </div>';
+      } else {
+         echo '<div class="alert alert-danger" role="alert"> Error </div>';
+      }
+      break;
+  }
+}
+
 $chiefs = selectChiefs();
 include "view-chiefs.php";
 include "view-footer.php";
