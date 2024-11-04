37c8ad0ff6c613ca3dee0c2fbd99f880c2639c43
bellagordon <157334313+bellagordon@users.noreply.github.com>
2024-11-04 13:29:22 -0600

    Update model-chiefs.php

--- model-chiefs.php
+++ model-chiefs.php
@@ -13,4 +13,46 @@ function selectChiefs() {
     }
 }
 
+function insertChief($chName, $chDpt) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("INSERT INTO `hw3_database`.`chief` (`patient_name`, `patient_description`) VALUES ( ?, ?)");
+        $stmt->bind_param("ss", $chName, $chDpt);
+        $success = $stmt->execute();
+        $conn->close();
+        return $success;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+
+function editChief($chName, $chDpt, $chid) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("UPDATE `hw3_database`.`chief` set `chief_name` = ?, `chief_dpt` = ?  WHERE chief_id =?");
+        $stmt->bind_param("ssi",$chName, $chDpt, $chid);
+        $success = $stmt->execute();
+        $conn->close();
+        return $success;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+
+function deleteChief($chid) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("DELETE FROM `hw3_database`.`chief` WHERE chief_id = ?");
+        $stmt->bind_param("i",$cid);
+        $success = $stmt->execute();
+        $conn->close();
+        return $success;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+
 ?>
