76dd2e37ee1f9d552c7c650686b97892589cf58b
bellagordon <157334313+bellagordon@users.noreply.github.com>
2024-11-06 10:19:56 -0600

    Update view-chiefs-newform.php

--- view-chiefs-newform.php
+++ view-chiefs-newform.php
@@ -16,8 +16,8 @@
       </div>
       <div class="modal-body">
         <form method="post" action="">
-            <div class="mb-3">s
-              <label for="chName" class="form-label">Head Surgeon Name</label>
+            <div class="mb-3">
+              <label for="chName" class="form-label"> Head Surgeon Name</label>
               <input type="text" class="form-control" id="chName" name="chName">
             </div>
            <div class="mb-3">
