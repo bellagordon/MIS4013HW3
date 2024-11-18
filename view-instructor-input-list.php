c95a152b323f5219d99332d104687f7886475121
bellagordon <157334313+bellagordon@users.noreply.github.com>
2024-11-18 13:32:47 -0600

    Create view-instructor-input-list.php

---
+++ view-instructor-input-list.php
@@ -0,0 +1,9 @@
+   <select class="form-select" id="iid" name="iid">
+  <?php
+  while ($instructorItem = $instructorList -> fetch_assoc()) {
+  ?>
+       <option value="<?php echo $instructorItem['doctor_id']; ?> "><?php echo $instructorItem['doctor_name']; ?> "</option>
+  <?php
+  }
+  ?>
+   </select> 
