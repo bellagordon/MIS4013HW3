a4323f24d11c97cd7f67374e2212abb0b59183f6
bellagordon <157334313+bellagordon@users.noreply.github.com>
2024-11-18 13:42:28 -0600

    Update view-instructor-input-list.php

--- view-instructor-input-list.php
+++ view-instructor-input-list.php
@@ -2,7 +2,7 @@
   <?php
   while ($instructorItem = $instructorList -> fetch_assoc()) {
   ?>
-       <option value="<?php echo $instructorItem['doctor_id']; ?> "><?php echo $instructorItem['doctor_name']; ?> </option>
+       <option value="<?php echo $instructorItem['doctor_id']; ?> "> <?php echo $instructorItem['doctor_name']; ?> </option>
   <?php
   }
   ?>
