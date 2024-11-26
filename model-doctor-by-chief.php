f48f93a3bf4cbd9875668ce1aaae410b96adf405
bellagordon <157334313+bellagordon@users.noreply.github.com>
2024-11-26 17:37:24 -0600

    Update model-doctor-by-chief.php

--- model-doctor-by-chief.php
+++ model-doctor-by-chief.php
@@ -1,9 +1,9 @@
 <?php
-function selectDoctorsbyChief($chid) {
+function selectDoctorsbyChief($iid) {
     try {
         $conn = get_db_connection();
         $stmt = $conn->prepare("SELECT i.doctor_id, doctor_name FROM instructor i join chief c on c.doctor_id = i.doctor_id where c.chief_id=?");
-        $stmt->bind_param("i", $chid);
+        $stmt->bind_param("i", $iid);
         $stmt->execute();
         $result = $stmt->get_result();
         $conn->close();
