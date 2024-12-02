ed8a7537181b2821937b2c5e01243c4de087d277
bellagordon <157334313+bellagordon@users.noreply.github.com>
2024-12-01 23:02:32 -0600

    Update model-doctor-by-chief.php

--- model-doctor-by-chief.php
+++ model-doctor-by-chief.php
@@ -14,28 +14,4 @@ function selectDoctorsbyChief($cid) {
     }
 }
 
-function selectInstructorsForInput() {
-    $conn = get_db_connection();
-    $stmt = $conn->prepare("SELECT doctor_id, doctor_name FROM instructor");
-    $stmt->execute();
-    $result = $stmt->get_result();
-    $conn->close();
-    return $result;
-}
-
-function updateChiefDoctor($chid, $iid) {
-    try {
-        $conn = get_db_connection();
-        $stmt = $conn->prepare("UPDATE chief SET doctor_id = ? WHERE chief_id = ?");
-        $stmt->bind_param("ii", $chid, $iid);
-        $stmt->execute();
-        $conn->close();
-        return $stmt->affected_rows; // Returns the number of affected rows
-    } catch (Exception $e) {
-        $conn->close();
-        throw $e;
-    }
-}
-
-
 ?>
