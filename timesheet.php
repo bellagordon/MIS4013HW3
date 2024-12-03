b679652a3311701d9727baa3a5688cda133dac4c
bellagordon <157334313+bellagordon@users.noreply.github.com>
2024-12-02 19:51:18 -0600

    Update timesheet.php

--- timesheet.php
+++ timesheet.php
@@ -1,27 +1,34 @@
 <html>
   <head>
-    <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
-    <script type="text/javascript">
-      google.charts.load('current', {'packages':['timeline']});
-      google.charts.setOnLoadCallback(drawChart);
-      function drawChart() {
-        var container = document.getElementById('timeline');
-        var chart = new google.visualization.Timeline(container);
-        var dataTable = new google.visualization.DataTable();
+<script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
 
-        dataTable.addColumn({ type: 'string', id: 'President' });
-        dataTable.addColumn({ type: 'date', id: 'Start' });
-        dataTable.addColumn({ type: 'date', id: 'End' });
-        dataTable.addRows([
-          [ 'Washington', new Date(1789, 3, 30), new Date(1797, 2, 4) ],
-          [ 'Adams',      new Date(1797, 2, 4),  new Date(1801, 2, 4) ],
-          [ 'Jefferson',  new Date(1801, 2, 4),  new Date(1809, 2, 4) ]]);
+<script type="text/javascript">
+  google.charts.load("current", {packages:["timeline"]});
+  google.charts.setOnLoadCallback(drawChart);
+  function drawChart() {
 
-        chart.draw(dataTable);
-      }
-    </script>
-  </head>
-  <body>
-    <div id="timeline" style="height: 180px;"></div>
-  </body>
-</html>
+    var container = document.getElementById('example5.1');
+    var chart = new google.visualization.Timeline(container);
+    var dataTable = new google.visualization.DataTable();
+    dataTable.addColumn({ type: 'string', id: 'Doctor' });
+    dataTable.addColumn({ type: 'string', id: 'Role' });
+    dataTable.addColumn({ type: 'date', id: 'Start' });
+    dataTable.addColumn({ type: 'date', id: 'End' });
+    dataTable.addRows([
+      [ 'Dr.Derek Shepherd', 'Craniotomy',             new Date(0,0,0,12,0,0),  new Date(0,0,0,14,30,0) ],
+      [ 'Dr.Derek Shepherd', 'Tumor Removal',          new Date(0,0,0,14,45,0),  new Date(0,0,0,15,30,0) ],
+      [ 'Dr.Meredith Grey', 'Clot Removal',            new Date(0,0,0,12,0,0),  new Date(0,0,0,13,0,0) ],
+      [ 'Dr.Christina Yang',   'Heart Transplant',      new Date(0,0,0,12,30,0), new Date(0,0,0,14,30,0) ],
+      [ 'Dr.Christina Yang',   'Heart Valve Replacement', new Date(0,0,0,15,0,0), new Date(0,0,0,16,30,0) ],
+      [ 'Dr.Owen Hunt',   'ER',                         new Date(0,0,0,12,0,0), new Date(0,0,0,15,0,0) ]]);
+
+    var options = {
+      timeline: { colorByRowLabel: true }
+    };
+
+    chart.draw(dataTable, options);
+  }
+
+</script>
+
+<div id="example5.1" style="height: 100px;"></div>
