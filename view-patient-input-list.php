3e426dac9c77907a220bf9290094ac7766cc87d0
bellagordon <157334313+bellagordon@users.noreply.github.com>
2024-12-03 00:12:05 -0600

    Update view-patient-input-list.php

--- view-patient-input-list.php
+++ view-patient-input-list.php
@@ -1,1 +1,15 @@
 
+<select class="form-select" id="iid_<?php echo $instructor['doctor_id']; ?>" name="iid">
+  <?php
+  while ($patientItem = $patientList -> fetch_assoc()) {
+     $selText = "";
+     if ($selectedPatient == $patientItem['nurse_id']) {
+        $selText = " selected";
+     }
+  ?>
+       <option value="<?php echo $patientrItem['nurse_id']; ?> " <?=$selText?> ><?php echo $patientItem['nurse_name']; ?> </option>
+  <?php
+  }
+  ?>
+   </select> 
+
