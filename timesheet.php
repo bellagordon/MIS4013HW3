6a745c7ec01ff2069d255d8a9b17540baff4c43c
bellagordon <157334313+bellagordon@users.noreply.github.com>
2024-12-02 20:43:35 -0600

    Update timesheet.php

--- timesheet.php
+++ timesheet.php
@@ -16,13 +16,13 @@
     dataTable.addColumn({ type: 'date', id: 'Start' });
     dataTable.addColumn({ type: 'date', id: 'End' });
     dataTable.addRows([
-      [ 'Dr.Derek Shepherd', 'Craniotomy',             new Date(0,0,0,12,0,0),  new Date(0,0,0,14,30,0) ],
-      [ 'Dr.Derek Shepherd', 'Tumor Removal',          new Date(0,0,0,14,45,0),  new Date(0,0,0,15,30,0) ],
-      [ 'Dr.Meredith Grey', 'Clot Removal',            new Date(0,0,0,12,0,0),  new Date(0,0,0,13,0,0) ],
-      [ 'Dr.Christina Yang',   'Heart Transplant',      new Date(0,0,0,12,30,0), new Date(0,0,0,14,30,0) ],
-      [ 'Dr.Christina Yang',   'Heart Valve Replacement', new Date(0,0,0,15,0,0), new Date(0,0,0,16,30,0) ],
-      [ 'Dr.Owen Hunt',   'ER',                         new Date(0,0,0,12,0,0), new Date(0,0,0,15,0,0) ]]);
-
+          [ '<b>Dr. Derek Shepherd</b>', 'Craniotomy', new Date(0, 0, 0, 12, 0, 0), new Date(0, 0, 0, 14, 30, 0) ],
+          [ '<b>Dr. Derek Shepherd</b>', 'Tumor Removal', new Date(0, 0, 0, 14, 45, 0), new Date(0, 0, 0, 15, 30, 0) ],
+          [ '<b>Dr. Meredith Grey</b>', 'Clot Removal', new Date(0, 0, 0, 12, 0, 0), new Date(0, 0, 0, 13, 0, 0) ],
+          [ '<b>Dr. Christina Yang</b>', 'Heart Transplant', new Date(0, 0, 0, 12, 30, 0), new Date(0, 0, 0, 14, 30, 0) ],
+          [ '<b>Dr. Christina Yang</b>', 'Heart Valve Replacement', new Date(0, 0, 0, 15, 0, 0), new Date(0, 0, 0, 16, 30, 0) ],
+          [ '<b>Dr. Owen Hunt</b>', 'ER', new Date(0, 0, 0, 12, 0, 0), new Date(0, 0, 0, 15, 0, 0) ]
+        ]);
     var options = {
       timeline: { colorByRowLabel: true }
     };
