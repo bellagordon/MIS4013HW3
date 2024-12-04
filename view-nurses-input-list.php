0f106b866cc6b6fc33f07b6b2cdb894ee8b94325
bellagordon <157334313+bellagordon@users.noreply.github.com>
2024-12-03 20:08:00 -0600

    Update view-nurses-input-list.php

--- view-nurses-input-list.php
+++ view-nurses-input-list.php
@@ -1,7 +1,6 @@
-<select class="form-select" id="cid_<?php echo $course['patient_id']; ?>" name="cid">    
-<?php while ($nurseItem = $nurseList->fetch_assoc()): ?>
-        <option value="<?php echo htmlspecialchars($nurseItem['nurse_id']); ?>" 
-            <?php echo ($selectedNurse == $nurseItem['nurse_id']) ? 'selected' : ''; ?>>
+<select class="form-select" id="sid" name="sid">
+    <?php while ($nurseItem = $nurseList->fetch_assoc()): ?>
+        <option value="<?php echo htmlspecialchars($nurseItem['nurse_id']); ?>">
             <?php echo htmlspecialchars($nurseItem['nurse_name']); ?>
         </option>
     <?php endwhile; ?>
