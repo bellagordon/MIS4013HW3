43e69584bcd3628983411a18e52198af8da5dcfd
bellagordon <157334313+bellagordon@users.noreply.github.com>
2024-12-03 18:53:25 -0600

    Update view-instructors2.php

--- view-instructors2.php
+++ view-instructors2.php
@@ -1,1 +1,29 @@
 
+<h1>Doctors</h1>
+<div class="table-responsive">
+  <table class="table">
+    <thead>
+      <tr>
+        <th>ID</th>
+      <th>Name</th>
+      <th>Office</th>
+        <th></th>
+      </tr>
+    </thead>
+    <tbody>
+      <?php
+while ($instructor = $instructors->fetch_assoc()) {
+  ?>
+  <tr>
+    <td><?php echo $instructor['doctor_id']; ?></td>
+    <td><?php echo $instructor['doctor_name']; ?></td>
+    <td><?php echo $instructor['office_number']; ?></td>
+<td><a href="courses-by-instructor.php?id=<?php echo $instructor['doctor_id']; ?>">Patients</a></td>
+  </tr>
+  <?php
+}
+        ?>
+    </tbody>
+  </table>
+</div>
+
