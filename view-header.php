fd60a1d54e1bfe8fe34e41650f41e5a5cc2f85cd
bellagordon <157334313+bellagordon@users.noreply.github.com>
2024-12-04 00:28:14 -0600

    Update view-header.php

--- view-header.php
+++ view-header.php
@@ -14,7 +14,9 @@
     <div class="container">
       <nav class="navbar navbar-expand-lg bg-body-tertiary">
   <div class="container-fluid">
-    <a class="navbar-brand" href="/">SGH</a>
+    <a class="navbar-brand" href="/">
+      <i class="bi bi-heart-pulse-fill"></i>
+    </a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
