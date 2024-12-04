7cdc65ff06f87d1378d56e7362cea7683fca7a55
bellagordon <157334313+bellagordon@users.noreply.github.com>
2024-12-03 19:47:55 -0600

    Create view-nurses-input-list.php

---
+++ view-nurses-input-list.php
@@ -0,0 +1,9 @@
+
+<select class="form-select" id="cid_<?php echo $course['patient_id']; ?>" name="cid">
+    <?php while ($nurseItem = $nurseList->fetch_assoc()): ?>
+        <option value="<?php echo htmlspecialchars($nurseItem['nurse_id']); ?>" 
+            <?php echo ($selectedNurse == $nurseItem['nurse_id']) ? 'selected' : ''; ?>>
+            <?php echo htmlspecialchars($nurseItem['nurse_name']); ?>
+        </option>
+    <?php endwhile; ?>
+</select>
