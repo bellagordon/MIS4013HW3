3f0d59dfea1b6f0fc0761a7310ede45420378f93
bellagordon <157334313+bellagordon@users.noreply.github.com>
2024-12-03 21:28:50 -0600

    Update view-header.php

--- view-header.php
+++ view-header.php
@@ -9,7 +9,7 @@
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
   </head>
   <body>
-    <div class="bg-success p-2 text-white bg-opacity-50">
+    <div class="bg-success p-2 text-white bg-opacity-25">
     <div class="container">
       <nav class="navbar navbar-expand-lg bg-body-tertiary">
   <div class="container-fluid">
@@ -19,9 +19,9 @@
       <span class="navbar-toggler-icon"></span>
     </button>
     </div>
+    <div class="bg-success p-2 text-white bg-opacity-50">
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
-      <div class="bg-success p-2 text-white bg-opacity-25">
-      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+          <ul class="navbar-nav me-auto mb-2 mb-lg-0">
         <li class="nav-item">
           <a class="nav-link active" aria-current="page" href="/">Home</a>
         </li>
@@ -36,7 +36,7 @@
         </li>
         </li>
       </ul>
-      </div>
+    </div>
     </div>
   </div>
 </nav>
