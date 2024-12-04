a7cfba96ebd77f5ff4e4315ebd2b77cea41eb8a5
bellagordon <157334313+bellagordon@users.noreply.github.com>
2024-12-03 21:21:48 -0600

    Update view-header.php

--- view-header.php
+++ view-header.php
@@ -17,6 +17,7 @@
       <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <div class="bg-success p-2 text-white bg-opacity-75">
       <ul class="navbar-nav me-auto mb-2 mb-lg-0">
         <li class="nav-item">
           <a class="nav-link active" aria-current="page" href="/">Home</a>
@@ -32,6 +33,7 @@
         </li>
         </li>
       </ul>
+      </div>
     </div>
   </div>
 </nav>
