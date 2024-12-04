6f8e07e62b4c6dfc79c4998c032d31f1b036f376
bellagordon <157334313+bellagordon@users.noreply.github.com>
2024-12-03 22:11:47 -0600

    Update index.php

--- index.php
+++ index.php
@@ -10,7 +10,7 @@ include "view-header.php";
     <h2>Hospital Database</h2>
   </div>
   <div class="card-body">
-    <h1 class="card-title fw-semibold">Seattle Grace Hospital</h1>
+    <h1 class="card-title fw-medium">Seattle Grace Hospital</h1>
     <h3 class="card-text fst-italic">Welcome to the centralized database for managing doctor and patient information</h3>
     <h6 class ="text-md-center fw-lighter">Start by navigating to the <a href="courses.php">Patients</a> or <a href="instructor.php">Doctors</a> sections.</h5>
   </div>
