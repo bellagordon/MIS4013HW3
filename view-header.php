4451880ddbf31b1d2baebec120a423417a3bda6e
bellagordon <157334313+bellagordon@users.noreply.github.com>
2024-12-03 21:19:03 -0600

    Update view-header.php

--- view-header.php
+++ view-header.php
@@ -3,14 +3,6 @@
 <!doctype html>
 <html lang="en">
   <head>
-    <style>
-    body, .container {  
-  background-color: #add8e6 !important; 
-    }
-      body, .container-fluid {  
-  background-color: #add8e6 !important; 
-    }
-    </style>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title> <?=$pageTitle?> </title>
