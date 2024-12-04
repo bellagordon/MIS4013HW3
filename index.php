b4798635806d50c5fb53c7d65d7bfc4826e94148
bellagordon <157334313+bellagordon@users.noreply.github.com>
2024-12-03 22:17:37 -0600

    Update index.php

--- index.php
+++ index.php
@@ -19,6 +19,14 @@ include "view-header.php";
   </div>
 </div>
 </div>
+<div class="card text-bg-dark">
+  <img src="https://www.google.com/url?sa=i&url=https%3A%2F%2Fen.wikipedia.org%2Fwiki%2FGrace_Hospital_%2528Seattle%2529&psig=AOvVaw1pc9jADTE3sTmoVnb4crup&ust=1733372234490000&source=images&cd=vfe&opi=89978449&ved=0CBQQjRxqFwoTCOC0x_SgjYoDFQAAAAAdAAAAABAE" class="card-img">
+  <div class="card-img-overlay">
+    <h5 class="card-title">Card title</h5>
+    <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+    <p class="card-text"><small>Last updated 3 mins ago</small></p>
+  </div>
+</div>
 <?php
  include "timesheet.php";
 include "view-footer.php";
