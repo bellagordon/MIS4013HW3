1bae75d269a105a9a962d9c9b7ef8501143e4376
bellagordon <157334313+bellagordon@users.noreply.github.com>
2024-12-04 00:15:48 -0600

    Update timesheet.php

--- timesheet.php
+++ timesheet.php
@@ -1,4 +1,5 @@
 
+
 <html>
   <head>
 <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
@@ -13,16 +14,15 @@
     var dataTable = new google.visualization.DataTable();
     dataTable.addColumn({ type: 'string', id: 'Doctor' });
     dataTable.addColumn({ type: 'string', id: 'Role' });
-    dataTable.addColumn({ type: 'string', id: 'Room' });
     dataTable.addColumn({ type: 'date', id: 'Start' });
     dataTable.addColumn({ type: 'date', id: 'End' });
     dataTable.addRows([
-      [ 'Dr.Derek Shepherd', 'Craniotomy', 'SG404',     new Date(0,0,0,12,0,0),  new Date(0,0,0,14,30,0) ],
-      [ 'Dr.Derek Shepherd', 'Tumor Removal', 'SG400',         new Date(0,0,0,14,45,0),  new Date(0,0,0,15,30,0) ],
-      [ 'Dr.Meredith Grey', 'Clot Removal', 'SG422',           new Date(0,0,0,12,0,0),  new Date(0,0,0,13,0,0) ],
-      [ 'Dr.Christina Yang',   'Heart Transplant', 'SG412',     new Date(0,0,0,12,30,0), new Date(0,0,0,14,30,0) ],
-      [ 'Dr.Christina Yang',   'Heart Valve Replacement', 'SG408', new Date(0,0,0,15,0,0), new Date(0,0,0,16,30,0) ],
-      [ 'Dr.Owen Hunt',   'ER',   'SG101',                      new Date(0,0,0,12,0,0), new Date(0,0,0,15,0,0) ]]);
+      [ 'Dr.Derek Shepherd', 'Craniotomy',             new Date(0,0,0,12,0,0),  new Date(0,0,0,14,30,0) ],
+      [ 'Dr.Derek Shepherd', 'Tumor Removal',          new Date(0,0,0,14,45,0),  new Date(0,0,0,15,30,0) ],
+      [ 'Dr.Meredith Grey', 'Clot Removal',            new Date(0,0,0,12,0,0),  new Date(0,0,0,13,0,0) ],
+      [ 'Dr.Christina Yang',   'Heart Transplant',      new Date(0,0,0,12,30,0), new Date(0,0,0,14,30,0) ],
+      [ 'Dr.Christina Yang',   'Heart Valve Replacement', new Date(0,0,0,15,0,0), new Date(0,0,0,16,30,0) ],
+      [ 'Dr.Owen Hunt',   'ER',                         new Date(0,0,0,12,0,0), new Date(0,0,0,15,0,0) ]]);
 
     var options = {
       timeline: { colorByRowLabel: true }
@@ -32,5 +32,4 @@
   }
 
 </script>
-    <div id="example5.1" style="height: 400px; width: 100%; font-weight: bold;"></div>
-<b></b>
+    <div id="example5.1" style="height: 400px; width: 100%;"></div>
