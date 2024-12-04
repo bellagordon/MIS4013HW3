b99ef6a879f22bdbd67d3967403708bd0da89906
bellagordon <157334313+bellagordon@users.noreply.github.com>
2024-12-04 02:10:39 -0600

    Create desktop-month-view.js

---
+++ desktop-month-view.js
@@ -0,0 +1,33 @@
+
+import * as mobiscroll from '@mobiscroll/javascript';
+
+export default {
+  // eslint-disable-next-line es5/no-shorthand-properties
+  init() {
+    var inst = mobiscroll.eventcalendar('#demo-desktop-month-view', {
+      // locale,
+      // theme,
+      // drag,
+      view: {
+        calendar: { labels: true },
+      },
+      onEventClick: function (args) {
+        mobiscroll.toast({
+          message: args.event.title,
+        });
+      },
+    });
+
+    mobiscroll.getJson(
+      'https://trial.mobiscroll.com/events/?vers=5',
+      function (events) {
+        inst.setEvents(events);
+      },
+      'jsonp',
+    );
+  },
+  // eslint-disable-next-line es5/no-template-literals
+  markup: `
+<div id="demo-desktop-month-view"></div>
+  `,
+};
