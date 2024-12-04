9e79d21605b406b3963055a813390f57ca600880
bellagordon <157334313+bellagordon@users.noreply.github.com>
2024-12-03 21:26:10 -0600

    Update view-header.php

--- view-header.php
+++ view-header.php
@@ -9,16 +9,18 @@
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
   </head>
   <body>
-    <div class="bg-success p-2 text-white bg-opacity-75">
+    <div class="bg-success p-2 text-white bg-opacity-50">
     <div class="container">
       <nav class="navbar navbar-expand-lg bg-body-tertiary">
   <div class="container-fluid">
+    <div class="bg-success p-2 text-white bg-opacity-50">
     <a class="navbar-brand" href="/">SGH</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
+    </div>
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
-      <div class="bg-success p-2 text-white bg-opacity-75">
+      <div class="bg-success p-2 text-white bg-opacity-25">
       <ul class="navbar-nav me-auto mb-2 mb-lg-0">
         <li class="nav-item">
           <a class="nav-link active" aria-current="page" href="/">Home</a>
