1029b09238c82a3770684da563de22876b2fb5be
bellagordon <157334313+bellagordon@users.noreply.github.com>
2024-12-03 21:46:14 -0600

    Update view-header.php

--- view-header.php
+++ view-header.php
@@ -9,7 +9,7 @@
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
   </head>
   <body>
-    <div class="bg-success p-2 text-dark bg-opacity-50">
+    <div class="p-3 mb-2 bg-info text-dark bg-opacity-50">
     <div class="container">
       <nav class="navbar navbar-expand-lg bg-body-tertiary">
   <div class="container-fluid">
