0552c79faab3256eecea04d20fd9a619bc949854
bellagordon <157334313+bellagordon@users.noreply.github.com>
2024-12-03 21:56:20 -0600

    Update index.php

--- index.php
+++ index.php
@@ -3,19 +3,20 @@
 $pageTitle = "Home";
 include "view-header.php";
 ?>
-
-<div class="card text-center">
+<div class="w-auto p-3">
+<div class="card text-center" style="width: 30rem; text">
   <div class="card-header">
     Hospital Database
   </div>
   <div class="card-body">
-    <h5 class="card-title">Seattle Grace Hospital</h5>
-    <p class="card-text">Welcome to the centralized database for managing doctor and patient information</p>
-    <p>Start by navigating to the <a href="courses.php">Patients</a> or <a href="instructor.php">Doctors</a> sections.</p>
+    <h1 class="card-title">Seattle Grace Hospital</h5>
+    <p class="card-text text-lg-center">Welcome to the centralized database for managing doctor and patient information</p>
+    <p class ="text-md-center">Start by navigating to the <a href="courses.php">Patients</a> or <a href="instructor.php">Doctors</a> sections.</p>
   </div>
-  <div class="card-footer text-body-secondary">
+  <div class="card-footer text-body-primary text-md-center">
           Today's Schedule
   </div>
+</div>
 </div>
        
 <?php
