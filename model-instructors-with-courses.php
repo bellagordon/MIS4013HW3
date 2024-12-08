04c0aba0331b62feb0d2329358c5bbe09e7554ac
bellagordon <157334313+bellagordon@users.noreply.github.com>
2024-12-08 16:52:31 -0600

    Update model-instructors-with-courses.php

--- model-instructors-with-courses.php
+++ model-instructors-with-courses.php
@@ -13,6 +13,20 @@ function selectInstructors() {
     }
 }
 
+function selectSections() {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("SELECT nurse_id, nurse_name FROM section");
+        $stmt->execute();
+        $result = $stmt->get_result();
+        $conn->close();
+        return $result;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+
 function selectCoursesbyinstructor($iid) {
     try {
         $conn = get_db_connection();
