08a6d69e52e1f41ff951291d8def1eb178712799
bellagordon <157334313+bellagordon@users.noreply.github.com>
2024-12-08 16:20:48 -0600

    Update model-courses-by-section.php

--- model-courses-by-section.php
+++ model-courses-by-section.php
@@ -1,6 +1,6 @@
 
 <?php
-function selectCoursesbysection($cid) {
+function selectCoursesbySection($cid) {
     try {
         $conn = get_db_connection();
         $stmt = $conn->prepare("SELECT c.patient_id, patient_name, patient_description, room, day_time FROM course c join section s on s.patient_id = c.patient_id where s.nurse_id=?");
