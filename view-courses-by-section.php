77440b709c6277fffd6803dbe86494b5a1740453
bellagordon <157334313+bellagordon@users.noreply.github.com>
2024-12-08 16:20:07 -0600

    Create view-courses-by-section.php

---
+++ view-courses-by-section.php
@@ -0,0 +1,33 @@
+
+<h1>Patients by Nurse</h1>
+<div class="table-responsive">
+  <table class="table">
+    <thead>
+      <tr>
+        <th>ID</th>
+      <th>Number</th>
+      <th>Description</th>
+        <th>Room</th>
+        <th>Day / Time </th>
+      
+
+      </tr>
+    </thead>
+    <tbody>
+      <?php
+while ($course = $courses->fetch_assoc()) {
+  ?>
+  <tr>
+    <td><?php echo $course['patient_id']; ?></td>
+    <td><?php echo $course['patient_name']; ?></td>
+    <td><?php echo $course['patient_description']; ?></td>
+    
+    <td><?php echo $course['room']; ?></td>
+    <td><?php echo $course['day_time']; ?></td>
+  </tr>
+  <?php
+}
+        ?>
+    </tbody>
+  </table>
+</div>
