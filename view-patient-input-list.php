737ab049e8c149b633043181e064c94cde244a5b
bellagordon <157334313+bellagordon@users.noreply.github.com>
2024-12-08 16:34:44 -0600

    Update view-patient-input-list.php

--- view-patient-input-list.php
+++ view-patient-input-list.php
@@ -1,8 +1,14 @@
+
 <select class="form-select" id="cid_<?php echo $instructor['doctor_id']; ?>" name="cid">
-    <?php while ($patientItem = $patientList->fetch_assoc()): ?>
-        <option value="<?php echo htmlspecialchars($patientItem['patient_id']); ?>" 
-            <?php echo ($selectedPatient == $patientItem['patient_id']) ? 'selected' : ''; ?>>
-            <?php echo htmlspecialchars($patientItem['patient_name']); ?>
-        </option>
-    <?php endwhile; ?>
-</select>
+  <?php
+  while ($patientItem = $patientList -> fetch_assoc()) {
+     $selText = "";
+     if ($selectedPatient == $patientItem['patient_id']) {
+        $selText = " selected";
+     }
+  ?>
+       <option value="<?php echo $patientItem['patient_id']; ?> " <?=$selText?> ><?php echo $patientItem['patient_name']; ?> </option>
+  <?php
+  }
+  ?>
+   </select> 
