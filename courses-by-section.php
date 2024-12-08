1e9add4e1511a851f96fdbb7fd7526505fd6be95
bellagordon <157334313+bellagordon@users.noreply.github.com>
2024-12-08 16:18:48 -0600

    Update courses-by-section.php

--- courses-by-section.php
+++ courses-by-section.php
@@ -4,7 +4,7 @@ require_once("model-courses-by-section.php");
 
 $pageTitle = "Patients by Nurse";
 include "view-header.php";
-$courses = selectCoursesbynurse($_GET['sid']);
+$courses = selectCoursesbynurse($_GET['cid']);
 include "view-courses-by-section.php";
 include "view-footer.php";
 ?>
