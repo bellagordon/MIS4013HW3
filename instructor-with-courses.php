ed22ff1647e73b2e6aa764a688d02b2c3f14e585
bellagordon <157334313+bellagordon@users.noreply.github.com>
2024-12-09 11:40:42 -0600

    Update instructor-with-courses.php

--- instructor-with-courses.php
+++ instructor-with-courses.php
@@ -5,7 +5,6 @@ require_once("model-instructors-with-courses.php");
 $pageTitle = "Doctors with Patients";
 include "view-header.php";
 $instructors = selectInstructors();
-$sections = selectNurses();
 include "view-instructors-with-courses.php";
 include "view-footer.php";
 ?>
