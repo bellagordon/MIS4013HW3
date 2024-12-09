e443388b9ec6aed3eb88679047010ff591193aa9
bellagordon <157334313+bellagordon@users.noreply.github.com>
2024-12-09 12:57:44 -0600

    Update view-instructors-with-courses.php

--- view-instructors-with-courses.php
+++ view-instructors-with-courses.php
@@ -1,7 +1,8 @@
 
-<?php include "view-instructors-with-courses-newform.php" ?>
+
 <h1 class="text-center">Doctors</h1>
-<div class="card-group" style="max-width: 800px; border: 2px solid #17a2b8; background-color: rgba(0, 123, 255, 0.1);">
+<div class="card-group">
+
   <?php
   while ($instructor = $instructors->fetch_assoc()) {
     ?>
@@ -25,6 +26,9 @@
                 <?php
               }
               ?>
+            <li> 
+              <?php include "view-instructors-with-courses-newform.php" ?>
+            </li>
             </ul>
           </div>
         </div>
@@ -35,7 +39,9 @@
   ?>
 </div>
 
-  <div class="d-flex justify-content-center mb-3">
+
+
+<div class="d-flex justify-content-center mb-3">
   <div class="card" style="max-width: 800px; border: 2px solid #17a2b8; background-color: rgba(0, 123, 255, 0.1);">
     <div class="card-body">
       <?php 
