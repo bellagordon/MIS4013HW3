81fb809d0d7964a2b5425a26c89d4fbd9d1bedfe
bellagordon <157334313+bellagordon@users.noreply.github.com>
2024-12-09 16:15:44 -0600

    Update instructor-with-courses.php

--- instructor-with-courses.php
+++ instructor-with-courses.php
@@ -10,21 +10,21 @@ if (isset($_POST['actionType'])) {
   switch ($_POST['actionType']) {
     case "Add":
       if (insertSection($_POST['iid'], $_POST['cid'], $_POST['sName'], $_POST['room'], $_POST['time'])) {
-        echo '<div class="alert alert-success" role="alert"> Staff member added! </div>';
+        echo '<div class="alert alert-success" role="alert"> Record added! </div>';
       } else {
          echo '<div class="alert alert-danger" role="alert"> Error </div>';
       }
       break;
      case "Edit":
       if (editSection($_POST['iid'], $_POST['cid'], $_POST['sName'], $_POST['room'], $_POST['time'], $_POST['sid'])) {
-        echo '<div class="alert alert-success" role="alert"> Staff member edited! </div>';
+        echo '<div class="alert alert-success" role="alert"> Record edited! </div>';
       } else {
          echo '<div class="alert alert-danger" role="alert"> Error </div>';
       }
       break;
      case "Delete":
       if (deleteSection($_POST['sid'])) {
-        echo '<div class="alert alert-success" role="alert"> Doctor deleted! </div>';
+        echo '<div class="alert alert-success" role="alert"> Record deleted! </div>';
       } else {
          echo '<div class="alert alert-danger" role="alert"> Error </div>';
       }
