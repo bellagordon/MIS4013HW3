6d4901542acbc63c741075495f4f7b8d4ea0453b
bellagordon <157334313+bellagordon@users.noreply.github.com>
2024-12-09 17:02:03 -0600

    Update view-instructors-with-courses.php

--- view-instructors-with-courses.php
+++ view-instructors-with-courses.php
@@ -23,6 +23,9 @@
                 <li class="list-group-item">
                   <?php echo $course['patient_name']; ?> - <?php echo $course['room']; ?> - <?php echo $course['day_time']; ?>
                 </li>
+                <li>
+                  <?php include "view-instructors-with-courses-editform.php"; ?>
+                </li>
               <li>
                  <form method="post" action="">
               <input type="hidden" name="sid" value="<?php echo $course['nurse_id']; ?>">
