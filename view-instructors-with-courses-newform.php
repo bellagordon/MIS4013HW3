52c713ca8272a45476f5afd889814ad838b5476b
bellagordon <157334313+bellagordon@users.noreply.github.com>
2024-12-10 16:41:14 -0600

    Update view-instructors-with-courses-newform.php

--- view-instructors-with-courses-newform.php
+++ view-instructors-with-courses-newform.php
@@ -15,7 +15,7 @@
   <div class="modal-dialog">
     <div class="modal-content">
       <div class="modal-header">
-        <h1 class="modal-title fs-5" id="newSectionModalLabel">New Section</h1>
+        <h1 class="modal-title fs-5" id="newSectionModalLabel">New Admittance Record</h1>
         <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
       </div>
       <div class="modal-body">
