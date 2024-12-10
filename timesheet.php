57087e941ace798b7aadaa9d14571b4e6907b387
bellagordon <157334313+bellagordon@users.noreply.github.com>
2024-12-10 13:38:52 -0600

    Update timesheet.php

--- timesheet.php
+++ timesheet.php
@@ -20,9 +20,18 @@
       [ 'Dr.Derek Shepherd', 'Craniotomy',             new Date(0,0,0,12,0,0),  new Date(0,0,0,14,30,0) ],
       [ 'Dr.Derek Shepherd', 'Tumor Removal',          new Date(0,0,0,14,45,0),  new Date(0,0,0,15,30,0) ],
       [ 'Dr.Meredith Grey', 'Clot Removal',            new Date(0,0,0,12,0,0),  new Date(0,0,0,13,0,0) ],
+       [ 'Dr.Miranda Bailey',   'Appendectomy',         new Date(0,0,0,13,30,0), new Date(0,0,0,15,0,0) ],
+       [ 'Dr.Miranda Bailey',   'Hernia Repair',         new Date(0,0,0,16,0,0), new Date(0,0,0,18,0,0) ],
+       [ 'Dr.Mark Sloan',   'Rhinoplasty',         new Date(0,0,0,13,0,0), new Date(0,0,0,16,0,0) ],
+       [ 'Dr.Mark Sloan',   'Meloplasty',         new Date(0,0,0,16,30,0), new Date(0,0,0,19,0,0) ],
+       [ 'Dr.Erica Hahn',   'Heart Bypass',         new Date(0,0,0,12,0,0), new Date(0,0,0,16,0,0) ],
       [ 'Dr.Christina Yang',   'Heart Transplant',      new Date(0,0,0,12,30,0), new Date(0,0,0,14,30,0) ],
       [ 'Dr.Christina Yang',   'Heart Valve Replacement', new Date(0,0,0,15,0,0), new Date(0,0,0,16,30,0) ],
-      [ 'Dr.Owen Hunt',   'ER',                         new Date(0,0,0,12,0,0), new Date(0,0,0,15,0,0) ]]);
+      [ 'Dr.Owen Hunt',   'ER / Trauma',                  new Date(0,0,0,12,0,0), new Date(0,0,0,15,0,0) ],
+      [ 'Dr.April Kepner',   'ER / Trauma',               new Date(0,0,0,15,0,0), new Date(0,0,0,20,0,0) ]);
+
+
+      
 
     var options = {
       timeline: { colorByRowLabel: true }
