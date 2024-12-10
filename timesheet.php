90d3b647474970710c49d9d8486f7b68dc3b5621
bellagordon <157334313+bellagordon@users.noreply.github.com>
2024-12-10 14:43:57 -0600

    Update timesheet.php

--- timesheet.php
+++ timesheet.php
@@ -33,7 +33,7 @@
        [ 'Dr.Erica Hahn',   'Heart Bypass',         new Date(0,0,0,12,0,0), new Date(0,0,0,16,0,0) ],
       [ 'Dr.Christina Yang',   'Heart Transplant',      new Date(0,0,0,12,30,0), new Date(0,0,0,14,30,0) ],
       [ 'Dr.Christina Yang',   'Heart Valve Replacement', new Date(0,0,0,15,0,0), new Date(0,0,0,16,30,0) ],
-      [ 'Dr.Owen Hunt',   'ER / Trauma',                  new Date(0,0,0,12,0,0), new Date(0,0,0,15,0,0) ],
+      [ 'Dr.Owen Hunt',   'ER / Trauma',                  new Date(0,0,0,12,0,0), new Date(0,0,0,16,0,0) ],
       [ 'Dr.April Kepner',   'ER / Trauma',               new Date(0,0,0,15,0,0), new Date(0,0,0,20,0,0)]]);
 
 
