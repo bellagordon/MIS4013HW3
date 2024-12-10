489dfd9d0728a75a0a1f798949cb115f57ec50a8
bellagordon <157334313+bellagordon@users.noreply.github.com>
2024-12-10 17:22:03 -0600

    Update index.php

--- index.php
+++ index.php
@@ -14,7 +14,7 @@ include "view-header.php";
   </div>
 </div>
 <div class="w-100 p-3">
-<div class="border border-light border-3 rounded-end">
+<div class="border border-info-subtle border-3 rounded-end">
   <div class="card text-center">
     <div class="card-header">
       <h2>Hospital Database</h2>
