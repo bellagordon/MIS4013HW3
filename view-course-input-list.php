87ec47d2d7edbd82c24f0642c1f5e14e2a6d095e
bellagordon <157334313+bellagordon@users.noreply.github.com>
2024-12-09 18:01:52 -0600

    Update view-course-input-list.php

--- view-course-input-list.php
+++ view-course-input-list.php
@@ -2,6 +2,10 @@
 <select class="form-select" id="cid" name="cid">
 <?php 
 while ($courseItem = $courseList -> fetch_assoc()) {
+   $selText = "";
+  if ($selectedCourse == $courseItem['patient_id']) {
+    $selText =" selected";
+  }
   ?>
   <option value="<?php echo $courseItem['patient_id']; ?>"><?php echo $courseItem['patient_name']; ?></option>
   <?php
