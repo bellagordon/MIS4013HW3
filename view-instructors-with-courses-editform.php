97186de134b51c429f36a42188b5b7646dc3a0dd
bellagordon <157334313+bellagordon@users.noreply.github.com>
2024-12-10 16:41:33 -0600

    Update view-instructors-with-courses-editform.php

--- view-instructors-with-courses-editform.php
+++ view-instructors-with-courses-editform.php
@@ -10,7 +10,7 @@
   <div class="modal-dialog">
     <div class="modal-content">
       <div class="modal-header">
-        <h1 class="modal-title fs-5" id="editSectionModalLabel<?php echo $course['doctor_id']; ?>">Edit Record</h1>
+        <h1 class="modal-title fs-5" id="editSectionModalLabel<?php echo $course['doctor_id']; ?>">Edit Admittance Record</h1>
         <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
       </div>
       <div class="modal-body">
