7da3fdd89241d09b57377a3fd9a801b619d89769
bellagordon <157334313+bellagordon@users.noreply.github.com>
2024-12-09 19:27:28 -0600

    Update courses.php

--- courses.php
+++ courses.php
@@ -9,7 +9,7 @@ include "view-header.php";
 if (isset($_POST['actionType'])) {
   switch ($_POST['actionType']) {
     case "Add":
-      if (insertCourse($_POST['cName'], $_POST['cDesc'], $_POST['cid'])) {
+      if (insertCourse($_POST['cName'], $_POST['cDesc'])) {
         echo '<div class="alert alert-success" role="alert"> Patient added! </div>';
       } else {
          echo '<div class="alert alert-danger" role="alert"> Error </div>';
