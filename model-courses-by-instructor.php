541f6c92c758dc5b80c01904a3a1b8e83a80f795
bellagordon <157334313+bellagordon@users.noreply.github.com>
2024-12-10 14:35:35 -0600

    Update model-courses-by-instructor.php

--- model-courses-by-instructor.php
+++ model-courses-by-instructor.php
@@ -3,7 +3,7 @@
 function selectCoursesbyInstructor($iid) {
     try {
         $conn = get_db_connection();
-        $stmt = $conn->prepare("SELECT c.patient_id, patient_name, patient_description, room, day_time, nurse_id, s.doctor_id FROM course c join section s on s.patient_id = c.patient_id join instructor i on s.doctor_id = i.doctor_id where s.doctor_id=?");
+        $stmt = $conn->prepare("SELECT c.patient_id, patient_name, patient_description, room, day_time, s.nurse_name, i.doctor_name FROM course c join section s on s.patient_id = c.patient_id join instructor i on s.doctor_id = i.doctor_id where s.doctor_id=?");
         $stmt->bind_param("i", $iid);
         $stmt->execute();
         $result = $stmt->get_result();
