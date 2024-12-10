9952e14860c939d5237e799810f98d673c28ba36
bellagordon <157334313+bellagordon@users.noreply.github.com>
2024-12-09 18:49:06 -0600

    Update view-instructors-with-courses.php

--- view-instructors-with-courses.php
+++ view-instructors-with-courses.php
@@ -2,10 +2,7 @@
 
 <!-- Container with blue background -->
 <div class="p-3 mb-2 bg-info text-dark bg-opacity-10" style="padding: 20px;">
-  
-  <!-- Add New Doctor Button -->
-  <div class="d-flex justify-content-between align-items-center" style="padding: 0 10px;">
-    <div></div> <!-- Empty div to balance the layout -->
+      <div></div> <!-- Empty div to balance the layout -->
     <?php include "view-instructors-with-courses-newform.php"; ?>  
   </div>
    
