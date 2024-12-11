1a969e5ddb8dbb448e9d6f930149cdedd412c851
bellagordon <157334313+bellagordon@users.noreply.github.com>
2024-12-10 19:29:47 -0600

    Update view-instructors.php

--- view-instructors.php
+++ view-instructors.php
@@ -33,7 +33,7 @@ while ($instructor = $instructors->fetch_assoc()) {
     <td><?php echo $instructor['doctor_id']; ?></td>
     <td><?php echo $instructor['doctor_name']; ?></td>
     <td><?php echo $instructor['office_number']; ?></td>
-     <td><a href="courses-by-instructor.php?iid=<?php echo $instructor['doctor_id']; ?>">All Patients</a></td>
+     <td><a href="courses-by-instructor.php?iid=<?php echo $instructor['doctor_id']; ?>">View Patients</a></td>
     <td>
         <?php 
   include "view-instructors-editform.php";
